8081764ed8aee2812d930d8220922e59914dac98
Daniel Ignat <daniel.ignat@ica.se>
2017-12-03 13:21:09 +0100

    Solution to challenge 2

--- 2/index.js
+++ 2/index.js
@@ -1,5 +1,37 @@
-// import fp from 'lodash/fp'
+import { reduce, sumBy, identity } from 'lodash/fp'
 
-export default function algorithm () {
-  return 0
+export default function algorithm (input) {
+  return sumNumbers(parseIntoMatrixOfNumbers(input).map(getBiggestDifference))
+}
+
+function sumNumbers (numbers) {
+  return sumBy(identity)(numbers)
+}
+
+function parseIntoMatrixOfNumbers (rawMatrix) {
+  return splitIntoRows(rawMatrix).map(row => splitIntoColumns(row).map(toInteger))
+}
+
+function getBiggestDifference (numbers) {
+  return max(numbers) - min(numbers)
+}
+
+function splitIntoRows (matrix) {
+  return matrix.split('\n')
+}
+
+function splitIntoColumns (row) {
+  return row.trimLeft().split(' ')
+}
+
+function toInteger (value) {
+  return parseInt(value, 10)
+}
+
+function max (numbers) {
+  return reduce((prev, current) => current > prev ? current : prev)(-Infinity)(numbers)
+}
+
+function min (numbers) {
+  return reduce((prev, current) => current < prev ? current : prev)(Infinity)(numbers)
 }
