85735d5b06573dead365a9148e4a8c328a81af81
Daniel Ignat <daniel.ignat@ica.se>
2017-12-03 12:42:32 +0100

    Solution to challenge 1

--- 1/index.js
+++ 1/index.js
@@ -1,4 +1,22 @@
+import fp from 'lodash/fp'
+
 export default function algorithm (numbers) {
-  const parsed = numbers.split().map(numberString => parseInt(numberString, 10))
-  return parsed
+  const parsed = numbers.split('').map(numberString => parseInt(numberString, 10))
+  return fp.reduce(sumUpMatchingSequences)(getMatchingSequenceIdentity(parsed))(parsed).sum
+}
+
+function sumUpMatchingSequences (previous, currentNumber) {
+  const matchesLastNumber = currentNumber === previous.previousNumber
+  const addition = matchesLastNumber ? currentNumber : 0
+  return {
+    sum: previous.sum + addition,
+    previousNumber: currentNumber
+  }
+}
+
+function getMatchingSequenceIdentity (numbers) {
+  return {
+    sum: 0,
+    previousNumber: numbers[numbers.length - 1]
+  }
 }
