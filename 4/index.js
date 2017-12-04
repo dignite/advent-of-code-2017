335b122a5c857c7005ddf82b3a1f0c136319fb7c
Daniel Ignat <daniel.ignat@ica.se>
2017-12-04 20:35:22 +0100

    Solution to challenge 4

--- 4/index.js
+++ 4/index.js
@@ -1,5 +1,38 @@
-// import { } from 'lodash/fp'
+import { reduce, some } from 'lodash/fp'
 
-export default function algorithm (position) {
-  return -1
+export default function algorithm (input) {
+  const rows = getRows(input)
+  return rows.length === 1
+    ? isValidPassPhrase(rows[0])
+    : rows.filter(row => isValidPassPhrase(row)).length
+}
+
+function isValidPassPhrase (passphrase) {
+  return !hasDuplicates(getWords(passphrase))
+}
+
+function getRows (input) {
+  return input.split('\n').map(dirtyRow => dirtyRow.trim())
+}
+
+function getWords (row) {
+  return row.split(' ')
+}
+
+function hasDuplicates (words) {
+  const countedWordFrequency = countWordFrequency(words)
+  return some(wordCount => wordCount > 1)(countedWordFrequency)
+}
+
+function countWordFrequency (words) {
+  return reduce((countByWord, currentWord) => {
+    const currentWordCount = countByWord[currentWord]
+    const newWordCount = currentWordCount
+      ? currentWordCount + 1
+      : 1
+    return {
+      ...countByWord,
+      [currentWord]: newWordCount
+    }
+  })({})(words)
 }
